878f9238215c0474bb23fb4211ca53988a9337c7
Artur <artur.prado@gmail.com>
2020-12-15 22:59:20 -0300

    Add sample input in puzzle 4

--- puzzle4/input.rb
+++ puzzle4/input.rb
@@ -984,3 +984,19 @@ INPUT = [
   'ecl:#ab45a8 byr:2009',
   'iyr:2025 pid:182cm hcl:d1614a cid:103'
 ]
+
+# INPUT = [
+#   'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
+#   'byr:1937 iyr:2017 cid:147 hgt:183cm',
+#   '',
+#   'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
+#   'hcl:#cfa07d byr:1929',
+#   '',
+#   'hcl:#ae17e1 iyr:2013',
+#   'eyr:2024',
+#   'ecl:brn pid:760753108 byr:1931',
+#   'hgt:179cm',
+#   '',
+#   'hcl:#cfa07d eyr:2025 pid:166559648',
+#   'iyr:2011 ecl:brn hgt:59in'
+# ]
