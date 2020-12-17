fdffd418c053efb436b15a3187880f212a65a170
Artur <artur.prado@gmail.com>
2020-12-17 13:20:00 -0300

    Add a solution for puzzle 13 (part 1)

---
+++ puzzle13/part1/solution.rb
@@ -0,0 +1,22 @@
+require_relative '../input.rb'
+
+timestamp = INPUT[0].to_i
+bus_lines = INPUT[1].split(',').select { |bus_line| bus_line != 'x' }.map(&:to_i)
+
+# eligible_lines = bus_lines.select { |bus_line| timestamp % bus_line == 0 }
+
+bus_found = false
+current_timestamp = timestamp
+while !bus_found
+  eligible_lines = bus_lines.select { |bus_line| timestamp % bus_line == 0 }
+  if eligible_lines.length > 0
+    bus_found = true
+    puts 'Bus lines:'
+    puts eligible_lines.inspect
+    puts "timestamp: #{current_timestamp}"
+    puts "answer: #{(timestamp - current_timestamp) * eligible_lines.first}"
+  else
+    timestamp += 1
+  end
+end
+
